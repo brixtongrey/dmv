de31fca2e551000d744caeb6d1ce6ffdc7faa0b6
Brixtongrey <brixtonaruda@gmail.com>
2025-10-28 18:30:57 -0600

    feat: created tables in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,16 @@ DROP TABLE IF EXISTS persons CASCADE;
 DROP TABLE IF EXISTS licenses;
 
 -- TODO: create "persons" and "licenses" tables
+CREATE TABLE persons (
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
+);
+
+CREATE TABLE licenses (
+    id SERIAL PRIMARY KEY,
+    address TEXT NOT NULL,
+    eye_color TEXT NOT NULL,
+    date_of_birth DATE NOT NULL,
+    person_id INTEGER UNIQUE NOT NULL REFERENCES persons(id) ON DELETE CASCADE
+);
